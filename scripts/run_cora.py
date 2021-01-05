152307fdd0a3ed7d085710788d14ebebee803f54
yuanqing-wang <wangyq@wangyq.net>
2021-01-05 14:38:45 -0500

    cora script

--- scripts/run_cora.py
+++ scripts/run_cora.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import dgl
 import stag
@@ -39,11 +40,11 @@ class Net(torch.nn.Module):
         )
 
         # middle layers: hidden -> hidden
-        for idx in range(1, depth-2):
+        for idx in range(1, depth-1):
             setattr(
                 self,
                 "gn%s" % idx,
-                getattr(
+                getattr(dgl_nn, layer)(
                     in_feats=hidden_features,
                     out_feats=hidden_features,
                     activation=activation
@@ -61,5 +62,92 @@ class Net(torch.nn.Module):
 
         return x
 
+def accuracy_between(y_pred, y_true):
+    if y_pred.dim() >= 2:
+        y_pred = y_pred.argmax(dim=-1)
+    return (y_pred == y_true).sum() / y_pred.shape[0]
 
+def run(args):
+    if args.stag == "normal":
+        dgl.function.sum = stag.stochastic_sum_normal(alpha=args.alpha)
+    
+    net = Net(
+        layer=args.layer,
+        in_features=1433,
+        out_features=7,
+        hidden_features=args.hidden_features,
+        activation=args.activation,
+        depth=args.depth,
+    )
 
+    optimizer = torch.optim.Adam(net.parameters(), args.lr)
+
+    ds = dgl.data.CoraGraphDataset()
+    g = ds[0]
+
+    if torch.cuda.is_available():
+        net = net.to('cuda:0')
+        g = g.to('cuda:0')
+
+    accuracy_tr = []
+    accuracy_te = []
+    accuracy_vl = []
+
+    for _ in range(args.n_epochs):
+        optimizer.zero_grad()
+        y_pred = net(g, g.ndata['feat'])[g.ndata['train_mask']]
+        y_true = g.ndata['label'][g.ndata['train_mask']]
+        loss = torch.nn.CrossEntropyLoss()(y_pred, y_true)
+        loss.backward()
+        optimizer.step()
+        
+        # training accuracy
+        accuracy_tr.append(accuracy_between(y_pred, y_true).cpu().detach().numpy())
+   
+        # test accuracy
+        y_pred = net(g, g.ndata['feat'])[g.ndata['test_mask']]
+        y_true = g.ndata['label'][g.ndata['test_mask']]
+        accuracy_te.append(accuracy_between(y_pred, y_true).cpu().detach().numpy())
+
+        
+        # validation accuracy
+        y_pred = net(g, g.ndata['feat'])[g.ndata['val_mask']]
+        y_true = g.ndata['label'][g.ndata['val_mask']]
+        accuracy_vl.append(accuracy_between(y_pred, y_true).cpu().detach().numpy())
+
+    accuracy_tr = np.array(accuracy_tr)
+    accuracy_te = np.array(accuracy_te)
+    accuracy_vl = np.array(accuracy_vl)
+
+    import os
+    os.mkdir(args.out)
+    np.save(args.out + "/accuracy_tr.npy", accuracy_tr)
+    np.save(args.out + "/accuracy_vl.npy", accuracy_vl)
+    np.save(args.out + "/accuracy_te.npy", accuracy_te)
+
+    import pandas as pd
+    df = pd.DataFrame.from_dict(
+        {
+            "accuracy_tr": [accuracy_tr[-1]],
+            "accuracy_vl": [accuracy_vl[-1]],
+            "accuracy_te": [accuracy_te[-1]],
+        }
+    )
+
+    df.to_markdown(args.out + "/overview.md")
+        
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out", type=str, default="out")
+    parser.add_argument("--depth", type=int, default=3)
+    parser.add_argument("--activation", type=str, default="elu")
+    parser.add_argument("--lr", type=float, default=1e-5)
+    parser.add_argument("--hidden_features", type=int, default=128)
+    parser.add_argument("--stag", type=str, default="none")
+    parser.add_argument("--alpha", type=float, default=0.2)
+    parser.add_argument("--layer", type=str, default="GraphConv")
+    parser.add_argument("--n_epochs", type=int, default=3000)
+    args = parser.parse_args()
+    run(args)
