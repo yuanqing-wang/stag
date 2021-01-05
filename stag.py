e2a9aeea48a3af0527a12e2eb4f5800c8da61aba
yuanqing-wang <wangyq@wangyq.net>
2021-01-05 16:17:18 -0500

    distribution speed up

--- stag.py
+++ stag.py
@@ -4,8 +4,18 @@ def stochastic_sum_normal(msg='m', out='h', alpha=0.2):
     def reducer(nodes):
         m = nodes.mailbox[msg]
         mask = torch.distributions.normal.Normal(
-            loc=torch.ones_like(m),
-            scale=torch.ones_like(m) * alpha,
-        ).sample()
+            loc=torch.tensor(1.0, device=m.device),
+            scale=torch.tensor(alpha, device=m.device),
+        ).sample(m.shape)
+        return {out: (m * mask).sum(dim=1)}
+    return reducer
+
+def stochastic_sum_uniform(msg='m', out='h', alpha=0.2):
+    def reducer(nodes):
+        m = nodes.mailbox[msg]
+        mask = torch.distributions.uniform.Uniform(
+            low=torch.tensor(1.0-alpha, device=m.device),
+            high=torch.tensor(1.0+alpha, device=m.device),
+        ).sample(m.shape)
         return {out: (m * mask).sum(dim=1)}
     return reducer
