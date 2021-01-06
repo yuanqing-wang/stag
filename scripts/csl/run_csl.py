3ddd92609596de2935a01795b53233cb50c6b79d
Yuanqing Wang <wangyq@wangyq.net>
2021-01-05 22:06:25 -0500

    csl scripts

--- scripts/csl/run_csl.py
+++ scripts/csl/run_csl.py
@@ -2,19 +2,19 @@ import numpy as np
 import torch
 import dgl
 import stag
-        
+
 class Net(torch.nn.Module):
     def __init__(
-            self, 
-            layer, 
-            in_features, 
-            hidden_features, 
-            out_features, 
-            depth, 
+            self,
+            layer,
+            in_features,
+            hidden_features,
+            out_features,
+            depth,
             activation=None
         ):
         super(Net, self).__init__()
-        
+
         # local import
         from dgl.nn import pytorch as dgl_nn
 
@@ -24,15 +24,15 @@ class Net(torch.nn.Module):
 
         # initial layer: in -> hidden
         self.gn0 = getattr(dgl_nn, layer)(
-            in_feats=in_features, 
+            in_feats=in_features,
             out_feats=hidden_features,
             activation=activation,
         )
 
         # last layer: hidden -> out
         setattr(
-            self, 
-            "gn%s" % (depth-1), 
+            self,
+            "gn%s" % (depth-1),
             getattr(dgl_nn ,layer)(
                 in_feats=hidden_features,
                 out_feats=out_features,
@@ -52,7 +52,7 @@ class Net(torch.nn.Module):
             )
 
         self.depth = depth
-        
+
     def forward(self, g, x):
         # ensure local scope
         g = g.local_var()
@@ -60,6 +60,8 @@ class Net(torch.nn.Module):
         for idx in range(self.depth):
             x = getattr(self, "gn%s" % idx)(g, x)
 
+        g.ndata["x"] = x
+        x = dgl.readout.sum_nodes(g, "x")
         return x
 
 def accuracy_between(y_pred, y_true):
@@ -67,28 +69,75 @@ def accuracy_between(y_pred, y_true):
         y_pred = y_pred.argmax(dim=-1)
     return (y_pred == y_true).sum() / y_pred.shape[0]
 
-def sampling_performance(g, net, n_samples=16):
-    y_pred = torch.stack([net(g, g.ndata['feat']) for _ in range(n_samples)], dim=0).mean(dim=0)
-    y_true = g.ndata['label']
-
-    _accuracy_tr = accuracy_between(y_pred[g.ndata['train_mask']], y_true[g.ndata['train_mask']]).item()
-    _accuracy_te = accuracy_between(y_pred[g.ndata['test_mask']], y_true[g.ndata['test_mask']]).item()
-    _accuracy_vl = accuracy_between(y_pred[g.ndata['val_mask']], y_true[g.ndata['val_mask']]).item()
-
-    return _accuracy_tr, _accuracy_te, _accuracy_vl
 
 def run(args):
+
+    import urllib
+    import os
+    if not os.exist("graphs_Kary_Deterministic_Graphs.pkl"):
+        urllib.urlretrieve(
+            "https://github.com/PurdueMINDS/RelationalPooling/raw/master/Synthetic_Data/graphs_Kary_Deterministic_Graphs.pkl",
+            "graphs_Kary_Deterministic_Graphs.pkl"
+        )
+
+        urllib.urlretrieve(
+            "https://github.com/PurdueMINDS/RelationalPooling/raw/master/Synthetic_Data/y_Kary_Deterministic_Graphs.pt",
+            "y_Kary_Deterministic_Graphs.pt"
+        )
+
+    import pickle
+    adjs = pickle.load(open("/content/graphs_Kary_Deterministic_Graphs.pkl", "rb"))
+    labels = torch.load("/content/y_Kary_Deterministic_Graphs.pt")
+    graphs = []
+    for adj in adjs:
+        g = dgl.DGLGraph()
+        g = dgl.from_scipy(adj)
+        graphs.append(g)
+
+    f_handle = open("idxs.txt", "r")
+    tr_line, te_line, vl_line = f_handle.readlines()
+    idx_tr = [int(x) for x in tr_line.split(",")]
+    idx_te = [int(x) for x in te_line.split(",")]
+    idx_vl = [int(x) for x in vl_line.split(",")]
+
+    g_tr = dgl.batch([graphs[idx] for idx in idx_tr])
+    g_te = dgl.batch([graphs[idx] for idx in idx_te])
+    g_vl = dgl.batch([graphs[idx] for idx in idx_vl])
+
+    y_tr = labels[idx_tr]
+    y_te = labels[idx_te]
+    y_vl = labels[idx_vl]
+
+    def sampling_performance(net, n_samples=16):
+        _accuracy_tr = accuracy_between(
+            net(g_tr, torch.zeros(g_tr.number_of_nodes(), 1, device=g.device)),
+            y_tr,
+        ).item()
+
+        _accuracy_te = accuracy_between(
+            net(g_te, torch.zeros(g_te.number_of_nodes(), 1, device=g.device)),
+            y_te,
+        ).item()
+
+        _accuracy_vl = accuracy_between(
+            net(g_vl, torch.zeros(g_vl.number_of_nodes(), 1, device=g.device)),
+            y_vl,
+        ).item()
+
+        return _accuracy_tr, _accuracy_te, _accuracy_vl
+
+
     from functools import partial
     if args.stag == "normal":
         dgl.function.sum = partial(stag.stochastic_sum_normal, alpha=args.alpha)
 
     elif args.stag == "uniform":
         dgl.function.sum = partial(stag.stochastic_sum_uniform, alpha=args.alpha)
-    
+
     net = Net(
         layer=args.layer,
-        in_features=1433,
-        out_features=7,
+        in_features=1,
+        out_features=10,
         hidden_features=args.hidden_features,
         activation=args.activation,
         depth=args.depth,
@@ -109,15 +158,15 @@ def run(args):
 
     for idx_epoch in range(args.n_epochs):
         optimizer.zero_grad()
-        y_pred = net(g, g.ndata['feat'])[g.ndata['train_mask']]
-        y_true = g.ndata['label'][g.ndata['train_mask']]
+        y_pred = net(g_tr, torch.zeros(g_tr.number_of_nodes(), 1, device=g_tr.device))
+        y_true = y_tr
         loss = torch.nn.CrossEntropyLoss()(y_pred, y_true)
         loss.backward()
         optimizer.step()
-        
+
         if idx_epoch % args.report_interval == 0:
             _accuracy_tr, _accuracy_te, _accuracy_vl = sampling_performance(g, net, n_samples=1)
-        
+
         accuracy_tr.append(_accuracy_tr)
         accuracy_te.append(_accuracy_te)
         accuracy_vl.append(_accuracy_vl)
@@ -133,7 +182,7 @@ def run(args):
     np.save(args.out + "/accuracy_te.npy", accuracy_te)
 
 
-    _accuracy_tr, _accuracy_te, _accuracy_vl = sampling_performance(g, net, n_samples=16) 
+    _accuracy_tr, _accuracy_te, _accuracy_vl = sampling_performance(g, net, n_samples=16)
 
 
     import pandas as pd
@@ -151,7 +200,7 @@ def run(args):
     df.to_markdown(open(args.out + "/overview.md", "w"))
 
     torch.save(net.to('cpu'), args.out + "/net.th")
-        
+
 
 if __name__ == "__main__":
     import argparse
