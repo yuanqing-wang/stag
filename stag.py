106e356067b807a824b36c6bba92b5702de8fe6b
yuanqing-wang <wangyq@wangyq.net>
2021-01-08 15:06:23 -0500

    prepare adaptive

--- stag.py
+++ stag.py
@@ -1,21 +1,33 @@
 import torch
 
-def stochastic_sum_normal(msg='m', out='h', alpha=0.2):
-    def reducer(nodes):
-        m = nodes.mailbox[msg]
-        mask = torch.distributions.normal.Normal(
-            loc=torch.tensor(1.0, device=m.device),
-            scale=torch.tensor(alpha, device=m.device),
-        ).sample(m.shape)
-        return {out: (m * mask).sum(dim=1)}
-    return reducer
+def stag_copy_src_normal(src='h', out='m', alpha=0.1):
+    if isinstance(alpha, float):
+        def message_fun(edges):
+            h = edges.src[src]
+            mask = torch.distributions.normal.Normal(
+                loc=torch.tensor(1.0, device=h.device),
+                scale=torch.tensor(alpha, device=h.device),
+            ).sample(h.shape)
+            return {out: (mask * h)}
+        return message_fun
 
-def stochastic_sum_uniform(msg='m', out='h', alpha=0.2):
-    def reducer(nodes):
-        m = nodes.mailbox[msg]
-        mask = torch.distributions.uniform.Uniform(
-            low=torch.tensor(1.0-alpha, device=m.device),
-            high=torch.tensor(1.0+alpha, device=m.device),
-        ).sample(m.shape)
-        return {out: (m * mask).sum(dim=1)}
-    return reducer
+def stag_copy_src_uniform(src='h', out='m', alpha=0.1):
+    if isinstance(alpha, float):
+        def message_fun(edges):
+            h = edges.src[src]
+            mask = torch.distributions.uniform.Uniform(
+                low=torch.tensor(1.0-alpha, device=h.device),
+                high=torch.tensor(1.0+alpha, device=h.device),
+            ).sample(h.shape)
+            return {out: (mask * h)}
+        return message_fun
+
+def stag_copy_src_bernoulli(src='h', out='m', alpha=0.1):
+    if isinstance(alpha, float):
+        def message_fun(edges):
+            h = edges.src[src]
+            mask = torch.distributions.bernoulli.Bernoulli(
+                probs=torch.tensor(1.0-alpha, device=h.device),
+            ).sample(h.shape)
+            return {out: (mask * h)}
+        return message_fun
