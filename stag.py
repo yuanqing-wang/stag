a4b655cec0db68b901609a9af27fcfc0df15dd5a
Yuanqing Wang <wangyq@umich.edu>
2021-02-11 18:09:58 -0500

    Update stag.py

--- stag.py
+++ stag.py
@@ -10,12 +10,11 @@ def stag_copy_src_vi(src="h", out="m"):
 def stag_copy_src_normal(src='h', out='m', alpha=0.1):
     def message_fun(edges):
         h = edges.src[src]
-        if h.shape[-1] >= 8:
-            mask = torch.distributions.normal.Normal(
-                loc=torch.tensor(1.0, device=h.device),
-                scale=torch.tensor(alpha, device=h.device),
-            ).sample(h.shape)
-            return {out: (mask * h)}
+        mask = torch.distributions.normal.Normal(
+            loc=torch.tensor(1.0, device=h.device),
+            scale=torch.tensor(alpha, device=h.device),
+        ).sample(h.shape)
+        return {out: (mask * h)}
         else:
             return {out: h}
     return message_fun
