22ffc1713a23cb8938a5468e207bedd72a0e5178
Yuanqing Wang <wangyq@wangyq.net>
2021-05-16 23:43:53 -0400

    transductive models

--- vi.py
+++ vi.py
@@ -325,3 +325,166 @@ class StagVI_NodeClassification_RC(StagVI):
 
         _g.ndata["x"] = x
         return _g, x
+
+class StagVI_NodeClassification_RE(StagVI):
+    def __init__(
+            self,
+            layer,
+            in_features,
+            hidden_features,
+            out_features,
+            depth,
+            activation=None,
+            a_prior=1.0,
+            kl_scaling=1.0,
+            a_mu_init_std=1.0,
+            a_log_sigma_init=0.0,
+        ):
+        super(StagVI_NodeClassification_RE, self).__init__(
+            layer=layer,
+            in_features=in_features,
+            hidden_features=hidden_features,
+            out_features=out_features,
+            depth=depth,
+            activation=activation,
+            a_prior=a_prior,
+            a_log_sigma_init=a_log_sigma_init,
+            kl_scaling=kl_scaling,
+        )
+
+        self.gn0_enc = layer(in_feats=in_features, out_feats=hidden_features, activation=activation)
+        self.gn1_enc = layer(in_feats=hidden_features, out_feats=hidden_features, activation=activation)
+
+        self.f_z_mu = torch.nn.Linear(2*hidden_features, 1)
+        self.f_z_log_sigma = torch.nn.Linear(2*hidden_features, 1)
+
+        torch.nn.init.normal_(self.f_z_log_sigma.weight, std=1e-3)
+        torch.nn.init.constant_(self.f_z_log_sigma.bias, a_log_sigma_init)
+
+        torch.nn.init.normal_(self.f_z_mu.weight, std=1e-2)
+        torch.nn.init.normal_(self.f_z_mu.bias, mean=1.0, std=a_mu_init_std)
+
+
+    def _forward(self, g, x):
+        """ Internal forward.
+        Parameters
+        ----------
+        g : dgl.DGLGraph
+            Input graph.
+        x : torch.Tensor
+            Input features. (n_nodes, in_features)
+        Returns
+        -------
+        dgl.DGLGraph
+            Graph with perturbed weights annotated.
+        """
+        _g = g.local_var()
+
+        _g.apply_edges(
+            lambda edges: {
+                **{
+                    "a0": torch.distributions.Normal(
+                        self.f_z_mu(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)),
+                        self.f_z_log_sigma(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
+                    ).rsample([self.in_features]).squeeze().transpose(1, 0),
+                },
+                **{
+                    "a%s" % idx: torch.distributions.Normal(
+                        self.f_z_mu(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)),
+                        self.f_z_log_sigma(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
+                    ).rsample([self.hidden_features]).squeeze().transpose(1, 0),
+                }
+            }
+        )
+
+        for idx in range(self.depth):
+            _g.edata["a"] = _g.edata["a%s" % idx]
+            x = getattr(self, "gn%s" % idx)(g, x)
+
+            if idx != self.depth - 1:
+                x = self.activation(x)
+
+        _g.ndata["x"] = x
+        return _g, x
+
+
+class StagVI_NodeClassification_REC(StagVI):
+    def __init__(
+            self,
+            layer,
+            in_features,
+            hidden_features,
+            out_features,
+            depth,
+            activation=None,
+            a_prior=1.0,
+            kl_scaling=1.0,
+            a_mu_init_std=1.0,
+            a_log_sigma_init=0.0,
+        ):
+        super(StagVI_NodeClassification_REC, self).__init__(
+            layer=layer,
+            in_features=in_features,
+            hidden_features=hidden_features,
+            out_features=out_features,
+            depth=depth,
+            activation=activation,
+            a_prior=a_prior,
+            a_log_sigma_init=a_log_sigma_init,
+            kl_scaling=kl_scaling,
+        )
+
+        self.gn0_enc = layer(in_feats=in_features, out_feats=hidden_features, activation=activation)
+        self.gn1_enc = layer(in_feats=hidden_features, out_feats=hidden_features, activation=activation)
+
+        self.f_z_mu = torch.nn.Linear(2*hidden_features, hidden_features)
+        self.f_z_log_sigma = torch.nn.Linear(2*hidden_features, hidden_features)
+
+        torch.nn.init.normal_(self.f_z_log_sigma.weight, std=1e-3)
+        torch.nn.init.constant_(self.f_z_log_sigma.bias, a_log_sigma_init)
+
+        torch.nn.init.normal_(self.f_z_mu.weight, std=1e-2)
+        torch.nn.init.normal_(self.f_z_mu.bias, mean=1.0, std=a_mu_init_std)
+
+
+    def _forward(self, g, x):
+        """ Internal forward.
+        Parameters
+        ----------
+        g : dgl.DGLGraph
+            Input graph.
+        x : torch.Tensor
+            Input features. (n_nodes, in_features)
+        Returns
+        -------
+        dgl.DGLGraph
+            Graph with perturbed weights annotated.
+        """
+        _g = g.local_var()
+
+        _g.apply_edges(
+            lambda edges: {
+                **{
+                    "a0": torch.distributions.Normal(
+                        self.f_z_mu_first(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)),
+                        self.f_z_log_sigma_first(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
+                    ).rsample(),
+                },
+                **{
+                    "a%s" % idx: torch.distributions.Normal(
+                        self.f_z_mu(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)),
+                        self.f_z_log_sigma(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
+                    ).rsample()
+                }
+            }
+        )
+
+        for idx in range(self.depth):
+            _g.edata["a"] = _g.edata["a%s" % idx]
+            x = getattr(self, "gn%s" % idx)(g, x)
+
+            if idx != self.depth - 1:
+                x = self.activation(x)
+
+        _g.ndata["x"] = x
+        return _g, x
