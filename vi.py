47879a746da613772a069999e4b4928e2ef4b910
Yuanqing Wang <wangyq@wangyq.net>
2021-05-16 22:47:56 -0400

    revert some

--- vi.py
+++ vi.py
@@ -67,56 +67,49 @@ class StagVI(torch.nn.Module):
         self.depth = depth
         self.kl_scaling = kl_scaling
 
+
     @staticmethod
     def condition(x):
         """ Condition (static method. )
-
         Takes a set of parameters and return a distribution.
-
         Parameters
         ----------
         x : torch.Tensor (n_graphs, out_features)
-
         Returns
         -------
         torch.distributions.Categorical
-
         """
         return torch.distributions.categorical.Categorical(logits=x)
 
     def loss(self, g, x, y, n_samples=1, mask=None):
         """ Training loss.
-
         Parameters
         ----------
         g : dgl.DGLGraph
             Input graph.
-
         x : torch.Tensor
             Input features.
-
         y : torch.Tensor
             Reference value.
-
         n_samples : int
             Number of samples to consider.
-
         Returns
         -------
         torch.Tensor : loss
-
         """
+
         # initialize losses
         losses = []
 
         for _ in range(n_samples):
+            _x = x
             # get input graph
             # x.shape = (n_graphs, out_features)
-            g, x = self._forward(g, x)
-            x = x[mask]
+            _g, _x = self._forward(g, _x)
+            _x = _x[mask]
 
             # posterior distribution
-            p_y_given_x_z = self.condition(x)
+            p_y_given_x_z = self.condition(_x)
 
             # prior
             p_a = self.a_prior
@@ -131,27 +124,24 @@ class StagVI(torch.nn.Module):
             elbo = p_y_given_x_z.log_prob(y[mask]).sum()\
                 + sum(
                     [
-                        p_a.log_prob(g.edata["a%s" % idx]).sum()\
-                        - q_a.log_prob(g.edata["a%s" % idx]).sum()
+                        p_a.log_prob(_g.edata["a%s" % idx]).sum()\
+                        - q_a.log_prob(_g.edata["a%s" % idx]).sum()
                         for idx in range(self.depth)
                     ]
                 )
 
             losses.append(-elbo)
 
-        return sum(losses) / len(losses)
+        return sum(losses) / n_samples
 
     def forward(self, g, x, n_samples=1, mask=None):
         """ Forward pass. (Inference Pass)
-
         Parameters
         ----------
         g : dgl.DGLGraph
             Input graph.
-
         x : torch.Tensor
             Input features.
-
         n_samples : int
             Number of samples to consider.
         """
@@ -159,11 +149,11 @@ class StagVI(torch.nn.Module):
 
         if mask is None:
             x = torch.stack(
-                [self._forward(_g, x)[1] for _ in range(n_samples)], dim=0
+                [self._forward(_g, x)[1].softmax(dim=-1) for _ in range(n_samples)], dim=0
             ).mean(dim=0)
         else:
             x = torch.stack(
-                [self._forward(_g, x)[1][mask] for _ in range(n_samples)], dim=0
+                    [self._forward(_g, x)[1][mask].softmax(dim=-1) for _ in range(n_samples)], dim=0
             ).mean(dim=0)
         return x
 
@@ -201,22 +191,19 @@ class StagVI_NodeClassification_R1(StagVI):
             torch.tensor(a_log_sigma_init)
         )
 
+
     def _forward(self, g, x):
         """ Internal forward.
-
         Parameters
         ----------
         g : dgl.DGLGraph
             Input graph.
-
         x : torch.Tensor
             Input features. (n_nodes, in_features)
-
         Returns
         -------
         dgl.DGLGraph
             Graph with perturbed weights annotated.
-
         """
         _g = g.local_var()
 
@@ -239,7 +226,9 @@ class StagVI_NodeClassification_R1(StagVI):
         for idx in range(self.depth):
             _g.edata["a"] = _g.edata["a%s" % idx]
             x = getattr(self, "gn%s" % idx)(g, x)
-            x = self.activation(x)
+
+            if idx != self.depth - 1:
+                x = self.activation(x)
 
         _g.ndata["x"] = x
         return _g, x
