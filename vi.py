68c75719eb1505aa28510298e59bd7d3a30d9720
Yuanqing Wang <wangyq@wangyq.net>
2021-05-26 00:45:21 -0400

    Update vi.py

--- vi.py
+++ vi.py
@@ -1,6 +1,5 @@
 import torch
 import dgl
-
 class StagVI(torch.nn.Module):
     def __init__(
             self,
@@ -73,7 +72,6 @@ class StagVI(torch.nn.Module):
         self.a_mu_init_std = a_mu_init_std
         self.a_log_sigma_init = a_log_sigma_init
 
-
     @staticmethod
     def condition(x):
         """ Condition (static method. )
@@ -112,16 +110,15 @@ class StagVI(torch.nn.Module):
         -------
         torch.Tensor : loss
         """
+        _g = g.local_var()
 
         # initialize losses
-        losses = []
+        neg_elbos = []
+
+        xs = []
 
         for _ in range(n_samples):
-            _x = x
-            # get input graph
-            # x.shape = (n_graphs, out_features)
-            _g, _x = self._forward(g, _x)
-            _x = _x[mask]
+            _g, _x = self._forward(_g, x)
 
             # posterior distribution
             p_y_given_x_z = self.condition(_x)
@@ -132,19 +129,31 @@ class StagVI(torch.nn.Module):
             # variational posterior
             q_a_array = self.q_a_array(_g)
 
-            # compute elbo
-            elbo = p_y_given_x_z.log_prob(y[mask]).sum()\
-                + self.kl_scaling * sum(
-                    [
-                        p_a.log_prob(_g.edata["a%s" % idx]).sum()\
-                        - q_a_array[idx].log_prob(_g.edata["a%s" % idx]).sum()
-                        for idx in range(self.depth)
-                    ]
-                )
+            # compute edge regularizer
+            _g.apply_edges(
+                lambda edge: {
+                    "reg": -sum(
+                        [
+                            p_a.log_prob(_g.edata["a%s" % idx]).sum(dim=-1)
+                            - q_a_array[idx].log_prob(_g.edata["a%s" % idx]).sum(dim=-1)\
+                            for idx in range(self.depth)
+                        ]
+                    )
+                }
+            )
 
-            losses.append(-elbo)
+            aggregate_fn = dgl.function.copy_e('reg', 'm_reg')
 
-        return sum(losses) / n_samples
+            # mean averages the regularization term
+            _g.update_all(aggregate_fn, dgl.function.mean(msg='m_reg', out='reg'))
+
+            neg_elbo_z = -p_y_given_x_z.log_prob(y)[mask].sum()\
+                + self.kl_scaling * _g.ndata['reg'][mask].sum()
+
+            neg_elbos.append(neg_elbo_z)
+
+        return sum(neg_elbos) / n_samples
+
 
     def forward(self, g, x, n_samples=1, mask=None):
         """ Forward pass. (Inference Pass)
@@ -158,7 +167,6 @@ class StagVI(torch.nn.Module):
             Number of samples to consider.
         """
         _g = g.local_var()
-
         if mask is None:
             x = torch.stack(
                 [self._forward(_g, x)[1].softmax(dim=-1) for _ in range(n_samples)], dim=0
@@ -168,6 +176,7 @@ class StagVI(torch.nn.Module):
                     [self._forward(_g, x)[1][mask].softmax(dim=-1) for _ in range(n_samples)], dim=0
             ).mean(dim=0)
         return x
+
 
 class StagVI_NodeClassification_R1(StagVI):
     def __init__(
@@ -245,7 +254,6 @@ class StagVI_NodeClassification_R1(StagVI):
         _g.ndata["x"] = x
         return _g, x
 
-
 class StagVI_NodeClassification_RC(StagVI):
     def __init__(
             self,
@@ -256,9 +264,9 @@ class StagVI_NodeClassification_RC(StagVI):
             depth,
             activation=None,
             a_prior=1.0,
-            kl_scaling=1.0,
+            a_log_sigma_init=1.0,
             a_mu_init_std=1.0,
-            a_log_sigma_init=0.0,
+            kl_scaling=1.0,
         ):
         super(StagVI_NodeClassification_RC, self).__init__(
             layer=layer,
@@ -271,6 +279,8 @@ class StagVI_NodeClassification_RC(StagVI):
             a_log_sigma_init=a_log_sigma_init,
             kl_scaling=kl_scaling,
         )
+
+        self.a_prior = torch.distributions.Normal(1.0, a_prior)
 
         # first layer
         self.a_mu_0 = torch.nn.Parameter(
@@ -311,7 +321,6 @@ class StagVI_NodeClassification_RC(StagVI):
             for idx in range(self.depth)
         ]
 
-
     def _forward(self, g, x):
         """ Internal forward.
         Parameters
@@ -325,8 +334,8 @@ class StagVI_NodeClassification_RC(StagVI):
         dgl.DGLGraph
             Graph with perturbed weights annotated.
         """
-        _g = g.local_var()
 
+        _g = g.local_var()
         _g.apply_edges(
             lambda edges: {
                 "a%s" % idx: torch.distributions.Normal(
@@ -339,13 +348,13 @@ class StagVI_NodeClassification_RC(StagVI):
 
         for idx in range(self.depth):
             _g.edata["a"] = _g.edata["a%s" % idx]
-            x = getattr(self, "gn%s" % idx)(g, x)
-            
+            x = getattr(self, "gn%s" % idx)(_g, x)
+
             if idx != self.depth - 1:
-                # x = getattr(self, "bn%s" % idx)(x)
                 x = self.activation(x)
 
         _g.ndata["x"] = x
+
         return _g, x
 
 
@@ -453,9 +462,9 @@ class StagVI_NodeClassification_REC(StagVI):
             depth,
             activation=None,
             a_prior=1.0,
-            kl_scaling=1.0,
+            a_log_sigma_init=1.0,
             a_mu_init_std=1.0,
-            a_log_sigma_init=0.0,
+            kl_scaling=1.0,
         ):
         super(StagVI_NodeClassification_REC, self).__init__(
             layer=layer,
@@ -469,18 +478,68 @@ class StagVI_NodeClassification_REC(StagVI):
             kl_scaling=kl_scaling,
         )
 
-        self.gn0_enc = layer(in_feats=in_features, out_feats=hidden_features, activation=activation)
-        self.gn1_enc = layer(in_feats=hidden_features, out_feats=hidden_features, activation=activation)
+        self.a_prior = torch.distributions.Normal(1.0, a_prior)
 
-        self.f_z_mu = torch.nn.Linear(2*hidden_features, hidden_features)
-        self.f_z_log_sigma = torch.nn.Linear(2*hidden_features, hidden_features)
+        self.gn0_enc = layer(in_feats=in_features, out_feats=hidden_features, activation=self.activation)
+        self.gn1_enc = layer(in_feats=hidden_features, out_feats=hidden_features, activation=self.activation)
 
-        torch.nn.init.normal_(self.f_z_log_sigma.weight, std=1e-3)
-        torch.nn.init.constant_(self.f_z_log_sigma.bias, a_log_sigma_init)
+        for idx in range(self.depth):
+            if idx == 0:
+                _hidden_features = in_features
+            else:
+                _hidden_features = hidden_features
 
-        torch.nn.init.normal_(self.f_z_mu.weight, std=1e-2)
-        torch.nn.init.normal_(self.f_z_mu.bias, mean=1.0, std=a_mu_init_std)
+            setattr(self, "f_z_mu_%s" % idx, torch.nn.Linear(2 * hidden_features, _hidden_features))
+            setattr(self, "f_z_log_sigma_%s" % idx, torch.nn.Linear(2 * hidden_features, _hidden_features))
 
+            # torch.nn.init.constant_(
+            #     getattr(self, "f_z_mu_%s" % idx).bias,
+            #     1.0
+            # )
+
+            # torch.nn.init.constant_(
+            #     getattr(self, "f_z_log_sigma_%s" % idx).bias,
+            #     a_log_sigma_init,
+            # )
+
+        self.a_mean_history = []
+        self.a_std_history = []
+
+    def q_a_array(self, g):
+        return [
+            torch.distributions.Normal(
+                loc=g.edata["mu_%s" % idx],
+                scale=g.edata["sigma_%s" % idx],
+            )
+            for idx in range(self.depth)
+        ]
+
+    def contrastive_loss(self, g):
+        _g = g.local_var()
+        fake_src_idxs = torch.multinomial(torch.ones(_g.number_of_nodes()), _g.ndata["train_mask"].sum(), replacement=True)
+        fake_dst_idxs = torch.multinomial(torch.ones(_g.number_of_nodes()), _g.ndata["train_mask"].sum(), replacement=True)
+
+        # fake_src_idxs = torch.randperm(_g.number_of_nodes())
+        # fake_dst_idxs = torch.randperm(_g.number_of_nodes())
+
+        return -sum(
+            [
+                torch.distributions.Normal(
+                    loc=getattr(self, "f_z_mu_%s" % idx)(torch.cat([_g.ndata["z_node"][fake_src_idxs, :], _g.ndata["z_node"][fake_dst_idxs, :]], dim=-1)),
+                    scale=getattr(self, "f_z_log_sigma_%s" % idx)(torch.cat([_g.ndata["z_node"][fake_src_idxs, :], _g.ndata["z_node"][fake_dst_idxs, :]], dim=-1)).exp(),
+                ).log_prob(torch.tensor(0.0)).sum()
+                for idx in range(self.depth)
+            ]
+        )
+        - sum(
+            [
+                torch.distributions.Normal(
+                    loc=g.edata["mu_%s" % idx],
+                    scale=g.edata["sigma_%s" % idx],
+                ).log_prob(torch.tensor(1.0)).sum()
+                for idx in range(self.depth)
+            ]
+        )
 
     def _forward(self, g, x):
         """ Internal forward.
@@ -497,29 +556,37 @@ class StagVI_NodeClassification_REC(StagVI):
         """
         _g = g.local_var()
 
+        z_node = self.gn0_enc(_g, x)
+        z_node = self.gn1_enc(_g, z_node)
+        _g.ndata["z_node"] = z_node
+
+        _g.apply_edges(lambda edges:{
+            **{
+                "mu_%s" % idx: getattr(self, "f_z_mu_%s" % idx)(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
+                for idx in range(self.depth)
+            },
+            **{
+                "sigma_%s" % idx: getattr(self, "f_z_log_sigma_%s" % idx)(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
+                for idx in range(self.depth)
+            }
+        }
+        )
+
         _g.apply_edges(
             lambda edges: {
-                **{
-                    "a0": torch.distributions.Normal(
-                        self.f_z_mu_first(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)),
-                        self.f_z_log_sigma_first(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
-                    ).rsample(),
-                },
-                **{
-                    "a%s" % idx: torch.distributions.Normal(
-                        self.f_z_mu(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)),
-                        self.f_z_log_sigma(torch.cat([edges.src["z_node"], edges.dst["z_node"]], dim=-1)).exp()
-                    ).rsample()
-                }
+                "a%s" % idx: torch.distributions.Normal(
+                    loc=edges.data["mu_%s" % idx],
+                    scale=edges.data["sigma_%s" % idx],
+                ).rsample()
+                for idx in range(self.depth)
             }
         )
 
         for idx in range(self.depth):
             _g.edata["a"] = _g.edata["a%s" % idx]
-            x = getattr(self, "gn%s" % idx)(g, x)
+            x = getattr(self, "gn%s" % idx)(_g, x)
 
-            if idx != self.depth - 1:
-                x = self.activation(x)
-
+        net.a_mean_history.append(_g.edata["a0"].mean().item())
+        net.a_std_history.append(_g.edata["a0"].std().item())
         _g.ndata["x"] = x
         return _g, x
