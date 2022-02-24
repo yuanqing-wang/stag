b9c4e53b7756235c7a287666576c2352da18a1fa
Yuanqing Wang <wangyq@wangyq.net>
2022-02-24 13:53:38 -0500

    cifar

--- stag/layers.py
+++ stag/layers.py
@@ -156,9 +156,6 @@ class StagLayer(torch.nn.Module):
         # kl_divergence = self.q_a.log_prob(edge_weight_sample).mean()\
         #    - self.p_a.log_prob(edge_weight_sample).mean()
 
-        print(self.q_a.base_distribution.mean)
-        print(self.q_a.base_distribution.stddev)
-
         kl_divergence = torch.distributions.kl_divergence(
             self.q_a.base_distribution,
             self.p_a.base_distribution,
@@ -176,6 +173,7 @@ class FeatOnlyLayer(torch.nn.Module):
         return self.layer(feat)
 
 class SumNodes(torch.nn.Module):
+    vi = False
     def __init__(self, name="to_sum"):
         super(SumNodes, self).__init__()
         self.name = name
