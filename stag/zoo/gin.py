becf394c1709851431e8b7220293db522b1d5051
yuanqing-wang <wangyq@wangyq.net>
2022-03-16 12:02:11 -0400

    gin

--- stag/zoo/gin.py
+++ stag/zoo/gin.py
@@ -3,8 +3,9 @@ import dgl
 
 class GIN(dgl.nn.GINConv):
     def __init__(
+        self,
         in_features, out_features,
-        *args, **kwargs
+        *args, **kwargs,
     ):
         apply_func = torch.nn.Linear(in_features, out_features)
-        super().__init__(apply_func=apply_func, * args, **kwargs)
+        super().__init__(apply_func=apply_func, *args, **kwargs)
